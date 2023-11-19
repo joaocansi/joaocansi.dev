bcd83a94493f1479068d126dffeea2e8d2656fc4
BuildTools <unconfigured@null.spigotmc.org>
2023-11-18 22:38:29 -0300

    fix: header width modified to fit like 90% of the screen

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,8 @@
 function Header() {
   return (
-    <header className="mx-auto flex h-24 max-w-screen-2xl items-center">
+    <header className="mx-auto flex h-24 w-[90%] max-w-screen-2xl items-center">
       <h1 className="w-[225px]">
-        <img className="w-full" src="/logo.png" alt="joaocansi.dev" />
+        <img className="ml-[-5px] w-full" src="/logo.png" alt="joaocansi.dev" />
       </h1>
     </header>
   )
