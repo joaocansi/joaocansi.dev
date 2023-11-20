b834511d6c76528b6d599c056c6d3a2e4db81c2c
BuildTools <unconfigured@null.spigotmc.org>
2023-11-20 09:57:08 -0300

    feat: add selection projects filtering

--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -1,4 +1,6 @@
 import IProject, { getTechnologies } from '@/shared/models/project'
+import { useEffect, useState } from 'react'
+import { getListSimilarity } from '@/shared/helpers'
 import Project from './project'
 import Section from '../section'
 import Selectable from '../selectable'
@@ -10,6 +12,44 @@ type ProjectsProps = {
 function Projects({ data }: ProjectsProps) {
   const technologies: string[] = getTechnologies(data)
 
+  const [projects, setProjects] = useState<IProject[]>(data)
+  const [isAllSelected, setIsAllSelected] = useState(true)
+  const [selectedTechnologies, setSelectedTechnologies] = useState<string[]>([])
+
+  useEffect(() => {
+    if (isAllSelected) setProjects(data)
+    else
+      setProjects(
+        data.filter(
+          (item) =>
+            getListSimilarity(selectedTechnologies, item.technologies).length >
+            0
+        )
+      )
+  }, [isAllSelected, selectedTechnologies])
+
+  const handleTechnologySelection = (technology: string) => {
+    if (selectedTechnologies.includes(technology))
+      setSelectedTechnologies((prev) => {
+        if (prev.length === 1) setIsAllSelected(true)
+        return prev.filter((item) => item !== technology)
+      })
+    else {
+      setSelectedTechnologies((prev) => [...prev, technology])
+      setIsAllSelected(false)
+    }
+  }
+
+  const handleAllSelection = () => {
+    if (!isAllSelected) {
+      setSelectedTechnologies([])
+      setIsAllSelected(true)
+    } else setIsAllSelected(false)
+  }
+
+  const isTechnologyActive = (technology: string) =>
+    !isAllSelected && selectedTechnologies.includes(technology)
+
   return (
     <Section
       id="projects"
@@ -17,14 +57,25 @@ function Projects({ data }: ProjectsProps) {
       description="These are the projects I made during my journey as a tech guy. Hope you like it!"
     >
       <div className="mt-5 flex flex-wrap justify-center gap-5 text-sm max-sm:mt-2 max-sm:text-xs">
+        <Selectable
+          active={isAllSelected}
+          key="technologies-0"
+          onClick={handleAllSelection}
+        >
+          All
+        </Selectable>
         {technologies.map((item, index) => (
-          <Selectable key={`technologies-${index}`} active={false}>
+          <Selectable
+            key={`technologies-${index}`}
+            active={isTechnologyActive(item)}
+            onClick={() => handleTechnologySelection(item)}
+          >
             {item}
           </Selectable>
         ))}
       </div>
       <div className="mt-16 flex flex-wrap justify-center gap-8">
-        {data.map((project, index) => (
+        {projects.map((project, index) => (
           <Project key={`project-${index}`} {...project} />
         ))}
       </div>
--- src/components/selectable.tsx
+++ src/components/selectable.tsx
@@ -3,14 +3,16 @@ import { ReactNode } from 'react'
 type SelectableProps = {
   children: ReactNode
   active: boolean
+  onClick: () => void
 }
 
-function Selectable({ children, active }: SelectableProps) {
+function Selectable({ children, active, onClick }: SelectableProps) {
   return (
     <span
       className={`cursor-pointer rounded-2xl bg-neutral-300 px-5 py-2 transition ${
         active ? 'bg-stone-400' : 'hover:bg-stone-400'
       }`}
+      onClick={onClick}
     >
       {children}
     </span>
